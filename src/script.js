b938520c275c3a46a59491bb2471c498bb7ebf9f
Siphesihle <sibiyasa24@gmail.com>
2024-10-03 23:36:35 +0200

    Added js for functionality.

--- src/script.js
+++ src/script.js
@@ -2,4 +2,6 @@ window.addEventListener('click', () =>
 
 let navLInks = document.querySelectorAll('nav a ul');
 const sections = document.querySelectorAll('sections');
-)
+) 
+
+const item = document.getElementById('demo')
